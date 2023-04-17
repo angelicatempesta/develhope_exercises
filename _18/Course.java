e9215126a9c54334a266a397159f1b00bf92e8c9
angelica tempesta <angelica.tempesta96@gmail.com>
2023-04-17 21:28:51 +0200

    esercizio 18/Creating our own types

---
+++ _18/Course.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Course {
+    String courseName = "English Course";
+    int maxStudents = 25;
+    int qualityRatingOutOf10 = 0;
+
+    List<String> studentNames = Arrays.asList("Alice", "Aragon", "Alex");
+    List<Integer> studentAges = Arrays.asList(23, 31, 38);
+
+}
+
+
+
+
